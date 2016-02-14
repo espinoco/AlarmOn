0b52c708ab5a073623d55d6c03af824e743b40f5
Carlo Rodríguez <carloeduardorodriguez@gmail.com>
2016-02-13 21:40:02 -0600

    Change the way of setting alarm for API level 19+

--- app/src/main/java/io/github/carlorodriguez/alarmon/PendingAlarmList.java
+++ app/src/main/java/io/github/carlorodriguez/alarmon/PendingAlarmList.java
@@ -15,14 +15,13 @@
 
 package io.github.carlorodriguez.alarmon;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.TreeMap;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 /**
  * This container holds a list of all currently scheduled alarms.
@@ -69,28 +68,14 @@ public final class PendingAlarmList {
     // Schedule the alarm with the AlarmManager.
     // Previous instances of this intent will be overwritten in
     // the alarm manager.
-    try {
-      // In API version 19 (KitKat), the set() method of is no longer
-      // guaranteed to have exact timing semantics.  A setExact()
-      // method is supplied, but is not available with the minimum SDK
-      // version used by this application.  Here we look for this
-      // new method and use it if we find it.  Otherwise, we fall back
-      // to the old set() method.
-      Method setExact = AlarmManager.class.getDeclaredMethod(
-          "setExact", int.class, long.class, PendingIntent.class);
-      setExact.invoke(alarmManager, AlarmManager.RTC_WAKEUP,
-          time.calendar().getTimeInMillis(), scheduleIntent);
-    } catch (NoSuchMethodException e) {
-      alarmManager.set(AlarmManager.RTC_WAKEUP,
-          time.calendar().getTimeInMillis(), scheduleIntent);
-    } catch (IllegalAccessException e) {
-      // TODO(cgallek) combine these all with the java 7 OR syntax.
-      throw new RuntimeException(e);
-    } catch (IllegalArgumentException e) {
-      throw new RuntimeException(e);
-    } catch (InvocationTargetException e) {
-      throw new RuntimeException(e);
-    }
+
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+          alarmManager.setExact(AlarmManager.RTC_WAKEUP,
+                  time.calendar().getTimeInMillis(), scheduleIntent);
+      } else {
+          alarmManager.set(AlarmManager.RTC_WAKEUP,
+                  time.calendar().getTimeInMillis(), scheduleIntent);
+      }
 
     // Keep track of all scheduled alarms.
     pendingAlarms.put(alarmId, new PendingAlarm(time, scheduleIntent));
